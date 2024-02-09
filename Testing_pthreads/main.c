84766abd0a6b1c35bb437fb06ea341eb6b287f34
= <=>
2024-02-09 12:48:43 -0800

    close to workingkeyboard and screens

--- Testing_pthreads/main.c
+++ Testing_pthreads/main.c
@@ -1,40 +1,91 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "list.h"
 
-void *print_message_function( void *ptr ){
-    char *message;
-    message = (char *) ptr;
-    for(int i = 0; i < 5; i++){
-        printf("%s: %d\n", message, i);
+#define MAX_BUFFER 256
+List* keyboard_list;
+pthread_mutex_t mutex;
+pthread_cond_t condition_empty;
+pthread_cond_t condition_full;
+
+
+void free_char(void* pItem) {
+  // cast pItem to a char pointer
+  char* i = (char*) pItem;
+  // free the memory allocated for the string
+  free(i);
+}
+
+void *print_to_screen(){
+    
+    char *message = "what?";
+    while(message[0] != '!'){
+
+        pthread_mutex_lock(&mutex);
+        if(List_count(keyboard_list)==0){
+            pthread_cond_wait(&condition_empty, &mutex);
+        }
+        message = List_trim(keyboard_list);
+        pthread_cond_signal(&condition_full);
+        pthread_mutex_unlock(&mutex);
+
+        printf("%s", message);
+        free(message);
     }
 }
 
 void *get_user_input(){
-    char input[80];
-    input[0] = ' ';
+    char *input = malloc(MAX_BUFFER);
+    *input = ' ';
     while(input[0] != '!'){
-        scanf("%s", input);
-        printf("%s\n", input);
+        fgets(input, MAX_BUFFER, stdin);
+        // input[MAX_BUFFER - 1] = '\0';
+        // printf("%ld", sizeof(input));
+
+        pthread_mutex_lock(&mutex);
+        if(List_count(keyboard_list)== LIST_MAX_NUM_NODES){
+            pthread_cond_wait(&condition_full, &mutex);
+        }
+        List_prepend(keyboard_list, (void*)input);
+        pthread_cond_signal(&condition_empty);
+        pthread_mutex_unlock(&mutex);
+
+                //printf("%s", input);
+        // *input = getchar();
+        // printf("%c", *input);
+        //printf("\n");
     }
 }
 
 int main() {
+    keyboard_list = List_create();
+    pthread_mutex_init(&mutex, NULL);
+    pthread_cond_init(&condition_empty, NULL);
+    pthread_cond_init(&condition_full, NULL);
     pthread_t thread1, thread2;
-    char *message1 = "Thread 1";
-    char *message2 = "Thread 2";
+    //char *message1 = "Thread 1";
+    //char *message2 = "Thread 2";
     int iret1, iret2;
     /* Create independent threads each of which will execute function */
+
+    // gets user input and but message in keyboard_list
     iret1 = pthread_create( &thread1, NULL, get_user_input, NULL);
-    // iret2 = pthread_create( &thread2, NULL, print_message_function, (void*) message2);
+
+    // get message from list and put on screen
+    iret2 = pthread_create( &thread2, NULL, print_to_screen, NULL);
+
     /* Wait till threads are complete before main continues. Unless we */
     /* wait we run the risk of executing an exit which will terminate */
     /* the process and all threads before the threads have completed. */
     pthread_join( thread1, NULL);
-    //pthread_join( thread2, NULL);
+    pthread_join( thread2, NULL);
     printf("Thread 1 returns: %d\n",iret1);
-    //printf("Thread 2 returns: %d\n",iret2);
+    printf("Thread 2 returns: %d\n",iret2);
+
+    FREE_FN free_er = free_char;
+    List_free(keyboard_list, free_er);
     exit(0);
     return 0;
 }
