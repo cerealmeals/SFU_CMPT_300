8ec821dded2bc8d74859749de927d8d05fe8f988
Kangyi02 <kza73@sfu.ca>
2024-03-13 22:35:44 -0700

    function structures

---
+++ assignment3/katies_test/Structures_functions.c
@@ -0,0 +1,64 @@
+#include "Structures_functions.h"
+
+// Copy the currently running process and put it on the ready
+// Q corresponding to the original process' priority.
+// Attempting to Fork the "init" process (see below) should fail.
+// Return pID on success, -1 on failure 
+int forkProcess()
+{
+    // if "init" process, return -1
+
+    // copy current process
+
+    // put it into the ready queue
+
+}
+
+// send a message to another process - block until reply
+// report scheduling information,  and reply source and text (once
+// reply arrives)
+// Return 0 on success, -1 on failure 
+int sendProcess(int pID, char* msg)
+{
+    // send a message
+
+    // block by calling P(S)
+
+    // print info
+
+    return -1;
+}
+
+// receive a message - block until one arrives
+// report: scheduling information and (once msg is received) the
+// message text and source of message
+void receiveProcess()
+{
+    // receive a message
+
+    // block by calling P(S)
+
+    // print info
+}
+
+// unblocks sender and delivers reply 
+// Return 0 on success, -1 on failure 
+int replyProcess(int pID, char* msg)
+{
+    // unblock sender, call V(S)
+
+    // deliver reply??
+}
+
+// dump complete state information of process to screen (this includes process
+// status and anything else you can think of)
+void procInfo(int pID)
+{
+
+}
+
+// display all process queues and their contents
+void totalInfo()
+{
+    
+}
---
+++ assignment3/katies_test/main.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include "list.h"
+#include "Structures_functions.h"
+
+int main()
+
+struct PCB init;
+init.ID = 0;
+init.priority = 0;
+init.state = RUNNING;
+// initialization stuff...
+// like the 5 list that make the queues...
+// 
+
+while(1) // not infinite but for now...
+{
+
+    // get user input
+    char c; // first letter of the scanf
+
+    scanf()
+    //break it into space separeted stuff...
+
+    
+    
+    switch(c){
+        case 'C' : // Sam
+            break;
+
+        case 'F' :  // Kaite
+            break;
+
+        case 'K' :  // Sam
+            break;
+
+        case 'E' :  // Sam
+            break;
+
+        case 'Q' :  // Sam
+            break;
+
+        case 'S' :  // Katie
+            break;
+
+        case 'R' :  // Katie
+            break;
+            
+        case 'Y' :  // Katie
+            break;
+
+        case 'N' :  // Sam
+            break;
+
+        case 'P' :  // Sam
+            break;
+
+        case 'V' :  // Sam
+            break;
+
+        case 'I' :  // Katie
+            break;
+
+        case 'T' :  // Katie
+            break;
+
+        default:    
+            // looping
+    }
+
+
+
+}
