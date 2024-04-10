2e4d48aadc5250a4b95064f67d9ff8aaca4d32b5
Kangyi02 <kza73@sfu.ca>
2024-04-10 10:32:39 -0700

    testing

--- assignment4/main.c
+++ assignment4/main.c
@@ -9,133 +9,95 @@
 
 int main(int argc, char *argv[])
 {
-    bool flag_i = false;
+     bool flag_i = false;
     bool flag_l = false;
-    // int arg1_len = 0;
-    // int arg2_len = 0;
 
+    bool path_flag = false;
     char path[PATH_MAX] = "\0";
-    //printf("path_max %d", PATH_MAX);
-    // char ch = '-';                                                                                                                                              
-    // char *ret1;
-    // char *ret2;
-
-    printf("argc #: %d\n", argc);
-
 
     if (argc < 2)
     {
         ls(path);
     }
-    // else if (argc == 2)
-    // {
-    //     if (strcmp(argv[1], "ls") == 0){
-    //         ls(path);
-    //     }
-    // }
-    else if(argc == 2)
-    {                                                                                                                                          
-        //arg1_len = strlen(argv[2]);
-        //ret1 = strchr(argv[2], ch);
-        //printf("String after |%c| is: |%s|\n", ch, ret1);
-        //printf("length of arg1: %d\n", arg1_len);
-
-        if (strcmp(argv[1], "-i") == 0)
-        {
-            flag_i = true;
-            printf("flag i is true\n");
-        }
-        else if (strcmp(argv[1], "-l") == 0)
-        {
-            flag_l = true;
-            printf("flag l is true\n");
-        }
-        else if (strcmp(argv[1], "-il") == 0 || strcmp(argv[1], "-li") == 0)
-        {
-            flag_i = true;
-            flag_l = true;
-            printf("flag i and l is true\n");
-        }
-        else
-        {
-            printf("Not the correct arguments, try 'i' or 'l'\n");
-        }
-    }
-    else if(argc == 3)
-    {
-        // arg1_len = strlen(argv[2]);
-        // arg2_len = strlen(argv[3]);
-        
-        // ret1 = strchr(argv[2], ch);
-        // ret2 = strchr(argv[3], ch);
-
-        // printf("String after |%c| is: |%s|\n", ch, ret1);
-        // printf("String after |%c| is: |%s|\n", ch, ret2);
-
-        if ((strcmp(argv[1], "-i")==0 && strcmp(argv[2], "-l")==0) || (strcmp(argv[1], "-l")==0 && strcmp(argv[2], "-i")==0))
-        {
-            flag_i = true;
-            flag_l = true;
-            printf("flag i and l is true\n");
-        }
-        else if (strcmp(argv[1], "-i")==0 && strcmp(argv[2], "-l")!=0)
-        {
-            // case: ./UnixLS ls -i path 
-            flag_i = true;
-            strcpy(path, argv[2]);
-            printf("flag i is true, getting path: %s\n", path);
-        }
-        else if (strcmp(argv[1], "-l")==0 && strcmp(argv[2], "-i")!=0)
-        {
-            // case: ./UnixLS ls -l path
-            flag_l = true;
-            strcpy(path, argv[2]);
-            printf("flag l is true, getting path: %s\n", path);
-        } 
-        else if (strcmp(argv[1], "-il")==0 || strcmp(argv[1], "-li")==0)
-        {
-            // case: ./UnixLS ls -il path or ./UnixLS ls -li path
-            flag_i = true;
-            flag_l = true;
-            strcpy(path, argv[2]);
-            printf("flag i and l is true, getting path: %s\n", path);
-        }
-        else
-        {
-            printf("Not the correct arguments, try 'i' or 'l' line 87\n");
-        }
-    }
-    else if(argc == 4)
-    {
-        if (strcmp(argv[1], "-i") == 0 && strcmp(argv[2], "-l") == 0)
-        {
-            flag_i = true;
-            flag_l = true;
-            strcpy(path, argv[3]);
-            printf("flag i and l is true, getting path: %s\n", path);
-        }
-        else if (strcmp(argv[1], "-l") == 0 && strcmp(argv[2], "-i") == 0)
-        {
-            flag_i = true;
-            flag_l = true;
-            strcpy(path, argv[3]);
-            printf("flag i and l is true, getting path: %s\n", path);
-        }
-    }
     else
     {
-        printf("Not the correct arguments, try 'i' or 'l' line 105\n");
-    }
+        for (int i = 1; i < argc; i++)
+        {
 
-    if (flag_i && !flag_l)
-    {
-        printf("lsi()\n");
-        lsi(path);
-    }
-    else if (!flag_i && flag_l)
-        lsl(path);
-    else if (flag_i && flag_l)
-        lsil(path);
+            // take the first argument
 
-    return 0;
+            // parse element
+            // check to see if it has '-' in front
+            // check letters
+            // if i or l set bool's to true
+            // if anything else return error
+
+            // if not starting with '-' it is a path
+            // give path to function
+            // set path_flag to flase
+            if (path_flag = false)
+            {
+                if (strcmp(argv[i], "-i") == 0)
+                {
+                    flag_i = true;
+                }
+                else if (strcmp(argv[i], "-l") == 0)
+                {
+                    flag_l = true;
+                }
+                else if (strcmp(argv[i], "-il") == 0)
+                {
+                    flag_i = true;
+                    flag_l = true;
+                }
+                else if (strcmp(argv[i], "-li") == 0)
+                {
+                    flag_i = true;
+                    flag_l = true;
+                }
+                else
+                {
+                    path_flag = true;
+                    strcpy(path, argv[i]);
+                    ls(path);
+                }
+            }
+            else
+            {
+                strcpy(path, argv[i]);
+                if (flag_i)
+                {
+                    lsi(path);
+                }
+                else if (flag_l)
+                {
+                    lsl(path);
+                }
+                else if (flag_i && flag_l)
+                {
+                    lsil(path);
+                }
+                else
+                {
+                    ls(path);
+                }
+            }
+        }
+
+        if (path_flag)
+        {
+            if (flag_i)
+            {
+                lsi();
+            }
+            else if (flag_l)
+            {
+                lsl();
+            }
+            else if (flag_i && flag_l)
+            {
+                lsil();
+            }
+        }
+    }
 }
